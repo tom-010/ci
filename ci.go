dafd3283b8f0454cfed01ea48b44990dfebb5a6f
Thomas Deniffel <tdeniffel@gmail.com>
2022-09-03 16:55:30 +0200

    working

--- ci.go
+++ ci.go
@@ -8,18 +8,51 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
 
-func main() {
-  cmd := exec.Command("./pipeline/01_abc.sh")
+// git rev-parse HEAD
+
+func gitHash() string {
+  stdout, _, err := run("git", "rev-parse", "HEAD")
+  if err != nil {
+    fmt.Println(err)
+    return ""
+  }
+  
+  return stdout 
+}
+
+
+func run(command string, args ...string) (string, string, error) {
+  cmd := exec.Command(command, args...)
   var outb, errb bytes.Buffer
   cmd.Stdout = &outb
   cmd.Stderr = &errb
   err := cmd.Run()
   if err != nil {
+    return "", "", err
+  }
+  return outb.String(), errb.String(), nil
+}
+
+func getName() string {
+  dt := time.Now()
+  res := dt.Format("2006-01-02_15:04:05")
+  h := gitHash()
+  if h != "" {
+    res += "_" + h
+  }
+  return res
+}
+
+func main() {
+  fmt.Println(getName())
+  stdout, stderr, err := run("./pipeline/01_abc.sh")
+  if err != nil {
     log.Fatal(err)
   }
-  fmt.Printf("out: %s\nerr: %s\n", outb.String(), errb.String())
+  fmt.Printf("out: %s\nerr: %s\n", stdout, stderr)
 
 
   fmt.Println("ci")
@@ -39,4 +72,6 @@ func main() {
       }
     }
   }
+  dt := time.Now()
+  fmt.Println(dt.Format("2006-02-01_15:04:05"))
 }
